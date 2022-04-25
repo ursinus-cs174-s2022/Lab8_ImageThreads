0143745aa35b76e3f6e1bd4a39c055329d925dde
Chris Tralie <chris.tralie@gmail.com>
2022-04-25 08:55:26 -0400

    Adding most of starter code

---
+++ imfilter.cpp
@@ -0,0 +1,164 @@
+#include <stdio.h>
+#include <cstring>
+#include <map>
+#include "simplecanvas/simplecanvas.h"
+
+
+/**
+ * @brief Return, by reference, the mean of the pixels in a neighborhood around a point
+ * 
+ * @param imagein 
+ * @param x 
+ * @param y 
+ * @param kernelWidth 
+ * @param pixelOut 
+ */
+void mean(SimpleCanvas& imagein, int x, int y, int kernelWidth, uint8_t* pixelOut) {
+    printf("MEAN\n");
+    double num = 0;
+    double total[3];
+    for (int k = 0; k < 3; k++) {
+        total[k] = 0;
+    }
+    for (int dy = -kernelWidth; dy <= kernelWidth; dy++) {
+        int y2 = y + dy;
+        for (int dx = -kernelWidth; dx <= kernelWidth; dx++) {
+            int x2 = x + dx;
+            if (x2 >= 0 && x2 < imagein.width && y2 >= 0 && y2 < imagein.height) {
+                num++;
+                for (int k = 0; k < 3; k++) {
+                    total[k] += imagein.data[y2][x2][k];
+                }
+            }
+        }
+    }
+    for (int k = 0; k < 3; k++) {
+        pixelOut[k] = (uint8_t)(total[k]/num);
+    }
+}
+
+void motionBlur(SimpleCanvas& imagein, int x, int y, int kernelWidth, uint8_t* pixelOut) {
+    double total[3];
+    for (int k = 0; k < 3; k++) {
+        total[k] = imagein.data[y][x][k];
+        if (x-kernelWidth >= 0 && y-kernelWidth >= 0) {
+            total[k] += imagein.data[y-kernelWidth][x-kernelWidth][k];
+        }
+        if (x+kernelWidth < imagein.width && y+kernelWidth < imagein.height) {
+            total[k] += imagein.data[y+kernelWidth][x+kernelWidth][k];
+        }
+    }
+    for (int k = 0; k < 3; k++) {
+        pixelOut[k] = (uint8_t)(total[k]/3);
+    }
+}
+
+void sharpen(SimpleCanvas& imagein, int x, int y, int kernelWidth, uint8_t* pixelOut) {
+    double total[3];
+    for (int k = 0; k < 3; k++) {
+        total[k] = imagein.data[y][x][k]*9;
+    }
+    for (int dx = -1; dx <= 1; dx++) {
+        for (int dy = -1; dy <= 1; dy++) {
+            int x2 = x + dx;
+            int y2 = y + dy;
+            if (dx == 0 && dy == 0) {
+                continue;
+            }
+            if (x2 >= 0 && x2 < imagein.width && y2 >= 0 && y2 < imagein.height) {
+                for (int k = 0; k < 3; k++) {
+                    total[k] -= (double)imagein.data[y2][x2][k];
+                }
+            }
+        }
+    }
+    for (int k = 0; k < 3; k++) {
+        if (total[k] > 255) {
+            total[k] = 255;
+        }
+        if (total[k] < 0) {
+            total[k] = 0;
+        }
+        pixelOut[k] = (uint8_t)total[k];
+    }
+}
+
+
+
+struct Parameters {
+    const char* inpath;
+    const char* outpath;
+    int kernelWidth=1;
+    void(*filter)(SimpleCanvas&, int, int, int, uint8_t*);
+};
+
+/**
+ * @brief Parse the command line arguments that specify parameters
+ *        for image processing
+ * 
+ * @param argc Number of command line arguments
+ * @param argv Array of command line arguments
+ * @return Parameters
+ */
+Parameters parseArgs(int argc, char** argv) {
+    Parameters params;
+    argv++, argc--;
+    while (argc > 0) {
+        if ((*argv)[0] == '-') {
+            if (strcmp(*argv, "--help") == 0) {
+                printf("Usage: ./imfilter --in <path to input> --out <path to output> --kernelWidth <width of kernel> --filter(mean/motionBlur)\n");
+                exit(0);
+            }
+            else if (!strcmp(*argv, "--in")) {
+                argv++; argc--;
+                params.inpath = (const char*)*argv;
+            }
+            else if (!strcmp(*argv, "--out")) {
+                argv++; argc--;
+                params.outpath = (const char*)*argv;
+            }
+            else if (!strcmp(*argv, "--kernelWidth")) {
+                argv++; argc--;
+                params.kernelWidth = atoi(*argv);
+            }
+            else if (!strcmp(*argv, "--mean")) {
+                params.filter = &mean;
+            }
+            else if (!strcmp(*argv, "--motionBlur")) {
+                params.filter = &motionBlur;
+            }
+            else if (!strcmp(*argv, "--sharpen")) {
+                params.filter = &sharpen;
+            }
+            else { 
+                fprintf(stderr, "Invalid option: %s\n", *argv);
+            }
+        }
+        argv++, argc--; 
+    }
+    return params;
+}
+
+
+void filterImage(SimpleCanvas& imagein, SimpleCanvas& imageout, Parameters params) {
+    uint8_t pixel[3];
+    for (int y = 0; y < imagein.height; y++) {
+        for (int x = 0; x < imagein.width; x++) {
+            printf("%i, %i\n", y, x);
+            printf("Calling filter\n");
+            params.filter(imagein, x, y, params.kernelWidth, pixel);
+            printf("Finished filter\n");
+            imageout.setPixel(x, y, pixel[0], pixel[1], pixel[2]);
+        }
+    }
+    
+}
+
+int main(int argc, char** argv) {
+    Parameters params = parseArgs(argc, argv);
+    SimpleCanvas imagein(params.inpath);
+    SimpleCanvas imageout(params.inpath);
+    filterImage(imagein, imageout, params);
+    imageout.write(params.outpath);
+    return 0;
+}
